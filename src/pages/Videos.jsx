3b5ece26caaab886a36a65d30d317f2743110458
Seokho <seokho0120@gmail.com>
2023-09-01 19:23:01 +0900

    ⚡️ 검색 헤더 구현

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 function Videos(props) {
-  return <div>Videos</div>;
+  const { keyword } = useParams();
+
+  return <div>Videos {keyword ? `🔎${keyword}` : '🔥'}</div>;
 }
 
 export default Videos;
