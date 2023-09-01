5edd7753a910ea1b0722cbcffa9c5ebad2deeacc
Seokho <seokho0120@gmail.com>
2023-09-01 16:37:58 +0900

    ⚡️ 초기 세팅, 라우터 설정

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import NotFound from './pages/NotFound';
+import Videos from './pages/Videos';
+import VideoDetail from './pages/VideoDetail';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Videos />,
+      },
+      {
+        path: 'videos',
+        element: <Videos />,
+        // 핫트렌드 비디오
+      },
+      {
+        path: 'videos/:keyword',
+        element: <Videos />,
+        // 검색한 핫트렌드 비디오
+      },
+      {
+        path: 'videos/watch/:videoId',
+        element: <VideoDetail />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
