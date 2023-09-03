7aba8a4975772391335dfba6a5205c04e6ccd40e
Seokho <seokho0120@gmail.com>
2023-09-03 20:36:10 +0900

    ⚡️ 상세페이지 영상플레이어 구현

---
+++ src/components/ChannelInfo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ChannelInfo({ name, id }) {
+  return <div>{name}</div>;
+}
+
+export default ChannelInfo;
--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import ChannelInfo from '../components/ChannelInfo';
+import RelatedVideos from '../components/RelatedVideos';
 
 function VideoDetail(props) {
-  return <div>VideoDetail</div>;
+  const {
+    state: { video },
+  } = useLocation();
+  const { title, channelId, channelTitle, description } = video.snippet;
+
+  return (
+    <section>
+      <article>
+        <iframe
+          id='player'
+          type='text/html'
+          width='100%'
+          height='640'
+          src={`http://www.youtube.com/embed/${video.id}`}
+          frameBorder='0'
+          title='video player'
+        />
+        <div>
+          <h2>{title}</h2>
+          <ChannelInfo id={channelId} name={channelTitle} />
+          <pre>{description}</pre>
+        </div>
+      </article>
+      <section>
+        <RelatedVideos id={video.id} />
+      </section>
+    </section>
+  );
 }
 
 export default VideoDetail;
