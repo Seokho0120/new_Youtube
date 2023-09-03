4a5c09e1a1ea0cd6d34797799bdf426375462b5c
Seokho <seokho0120@gmail.com>
2023-09-03 20:16:28 +0900

    ⚡️ 비디오카드 스타일링

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -41,11 +41,11 @@ function Videos() {
 
   return (
     <>
-      <div>Videos {keyword ? `🔎${keyword}` : '🔥'}</div>;
+      <div>Videos {keyword ? `🔎${keyword}` : '🔥'}</div>
       {isLoading && <p>Loading...</p>}
       {error && <p>Something is wrong!!!!</p>}
       {videos && (
-        <ul>
+        <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4'>
           {videos.map((video) => (
             <VideoCard key={video.id} video={video} />
           ))}
